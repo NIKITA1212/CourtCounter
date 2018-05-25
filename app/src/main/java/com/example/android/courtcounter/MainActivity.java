8c370dc68eb2617958d73f73ec0de87168aca144
nikita <nikitatank1212@csu.fullerton.edu>
2018-05-24 23:35:10 -0700

    change the size of image to 1024 X 768

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -22,15 +22,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-/**
- * This activity keeps track of the basketball score for 2 teams.
- */
 public class MainActivity extends AppCompatActivity {
 
-    // Tracks the score for Team A
     int scoreTeamA = 0;
 
-    // Tracks the score for Team B
     int scoreTeamB = 0;
 
     @Override
@@ -39,57 +34,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * Increase the score for Team A by 1 point.
-     */
     public void addOneForTeamA(View v) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Increase the score for Team A by 2 points.
-     */
     public void addTwoForTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Increase the score for Team A by 3 points.
-     */
     public void addThreeForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
-    /**
-     * Increase the score for Team B by 1 point.
-     */
     public void addOneForTeamB(View v) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Increase the score for Team B by 2 points.
-     */
     public void addTwoForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Increase the score for Team B by 3 points.
-     */
     public void addThreeForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Resets the score for both teams back to 0.
-     */
     public void resetScore(View v) {
         scoreTeamA = 0;
         scoreTeamB = 0;
@@ -97,17 +71,11 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamB(scoreTeamB);
     }
 
-    /**
-     * Displays the given score for Team A.
-     */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    /**
-     * Displays the given score for Team B.
-     */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
